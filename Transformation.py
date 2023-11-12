a0fc69f68c4c1febe35fd5b78776c625b70cb31a
Eelaazmi <elaazmielmahdi@gmail.com>
2023-11-13 00:51:03 +0100

    All masks are done

--- Transformation.py
+++ Transformation.py
@@ -8,31 +8,101 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import sys
-def nothing(x):
-    # Load an image
-    image = cv2.imread('../images/Apple_Black_rot/image (1).JPG')
-    grayscale_image = pcv.rgb2gray(image)
-
-    # Image transformations and analysis
-    # Gaussian Blur
-    blurred_image = pcv.gaussian_blur(image, ksize=(5, 5), sigma_x=0)
-
-    # Mask
-    # You can create a mask based on your specific requirements. Here's an example:
-    mask = pcv.threshold.binary(grayscale_image, threshold=80)
-
-    plt.subplot(2, 3, 2)
-    plt.imshow(blurred_image)
-    plt.title('Gaussian Blur')
-
-    plt.subplot(2, 3, 3)
-    plt.imshow(mask, cmap='gray')
-    plt.title('Mask')
 
 def usage():
     print("Img Usage: python3 Transformation.py <path_to_image>")
     print("Dir Usage: ./Transformation.[extension] -src Apple/apple_healthy/ -dst dst_directory")
     exit(1)
+
+def ft_Pseudolandmarks(image):
+    # Convert the Image to HSV for better color analysis
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # Define the Color Range for Object Detection
+    lower_color = np.array([40, 40, 40])
+    upper_color = np.array([80, 255, 255])
+    # Create a Mask Based on the Color Range
+    color_mask = cv2.inRange(hsv_image, lower_color, upper_color)
+    # Find Contours of Objects in the Mask
+    contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    pseudolandmarks = []
+    for contour in contours:
+        # Calculate the bounding box of the object
+        x, y, w, h = cv2.boundingRect(contour)
+        # Calculate the center of the bounding box as a pseudolandmark
+        center_x = x + w // 2
+        center_y = y + h // 2
+        pseudolandmarks.append((center_x, center_y))
+    # Draw Pseudolandmarks on the Original Image
+    result_image = image.copy()
+    for landmark in pseudolandmarks:
+        cv2.circle(result_image, landmark, 5, (0, 255, 0), -1)
+    plt.subplot(2, 6, 12), plt.imshow(result_image[:, :, ::-1]), plt.title('Pseudolandmarks')
+
+def ft_analyze_objects(image):
+    # Convert the Image to HSV for better color analysis
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # Define the Color Range for Object Detection
+    lower_color = np.array([40, 40, 40])
+    upper_color = np.array([80, 255, 255])
+    # Create a Mask Based on the Color Range
+    color_mask = cv2.inRange(hsv_image, lower_color, upper_color)
+    # Find Contours of Objects in the Mask
+    contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Draw Contours on a Blank Canvas
+    contour_canvas = np.zeros_like(image, dtype=np.uint8)
+    cv2.drawContours(contour_canvas, contours, -1, (0, 255, 0), 2)
+    # Draw Lines Inside the Detected Objects
+    line_color = (0, 0, 255)
+    line_thickness = 2
+    for contour in contours:
+        # Convert the contour to a numpy array
+        contour_array = contour.reshape((-1, 2))
+        for i in range(len(contour_array) - 1):
+            cv2.line(contour_canvas, tuple(contour_array[i]), tuple(contour_array[i + 1]), line_color, line_thickness)
+        cv2.line(contour_canvas, tuple(contour_array[-1]), tuple(contour_array[0]), line_color, line_thickness)
+    # Overlay the Contours on the Original Image
+    result_image = cv2.addWeighted(image, 1, contour_canvas, 1, 0)
+    plt.subplot(2, 6, 11), plt.imshow(result_image[:, :, ::-1]), plt.title('Analyze Objects')
+
+def ft_roi_objects(image):
+        # Define the Color to Fill
+    target_color = [100, 90, 80]
+        # Define a Color Similarity Threshold
+    color_similarity_threshold = 50  # Adjust as needed
+        # Create a Mask Based on Color Similarity
+    color_difference = np.abs(image - target_color)
+    color_mask = np.all(color_difference <= color_similarity_threshold, axis=-1)
+        # Define the Fill Color
+    fill_color = [0, 255, 0]
+    result_image = image.copy()
+    result_image[color_mask] = fill_color
+    plt.subplot(2, 6, 10), plt.imshow(result_image[:, :, ::-1]), plt.title('Roi Objects')
+
+def ft_color_mask(image):
+    h_min = 70
+    h_max = 255
+    s_min = 70
+    s_max = 255
+    v_min = 70
+    v_max = 255
+    lower_bound = np.array([h_min, s_min, v_min], dtype=np.uint8)
+    upper_bound = np.array([h_max, s_max, v_max], dtype=np.uint8)
+    color_mask = cv2.inRange(image, lower_bound, upper_bound)
+    result = cv2.bitwise_and(image, image, mask=~color_mask)
+    plt.subplot(2, 6, 8), plt.imshow(result), plt.title('Color Mask')
+    # [] COLOR ALPHA MASK
+        # Invert the Mask to Create an Alpha Channel
+    alpha_channel = 255 - color_mask
+        # Merge RGB Channels with Alpha Channel
+    filtered_image = cv2.merge((image, alpha_channel))
+    plt.subplot(2, 6, 9), plt.imshow(filtered_image), plt.title('Color Alpha Mask')
+
+def ft_rgb_mask(image, grayscale_image):
+    thresh = 122
+    _, binary_mask = cv2.threshold(grayscale_image, thresh, 255, cv2.THRESH_BINARY)
+    refined_mask = cv2.erode(binary_mask, None, iterations=1)
+    refined_mask = cv2.dilate(refined_mask, None, iterations=1)
+    plt.subplot(2, 6, 7), plt.imshow(refined_mask), plt.title('Refined Mask')
 
 def transform_image(img_path):
     #  image transformation 
@@ -42,51 +112,39 @@ def transform_image(img_path):
     # Convert the Image to RGB
     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # Convert the Image to Grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # PLOT CANVAS
-    plt.figure(figsize=(9, 9))
-
+    plt.figure(figsize=(12, 8))
     # [X] ORIGINAL IMAGE
-    plt.subplot(3, 4, 1), plt.imshow(image), plt.title('Original Image')
-
+    plt.subplot(2, 6, 1), plt.imshow(image), plt.title('Original Image')
     # [X] GRAYSCALE IMAGE
-    plt.subplot(3, 4, 2), plt.imshow(gray_image, cmap='gray'), plt.title('Gray Image')
-
+    plt.subplot(2, 6, 2), plt.imshow(grayscale_image, cmap='gray'), plt.title('Gray Image')
     # [X] GAUSSIAN BLUR
     kernel_size = (5, 5)
-    GB_image = cv2.GaussianBlur(gray_image, kernel_size, 0)
-    plt.subplot(3, 4, 3), plt.imshow(GB_image), plt.title('Gaussian blur')
-    plt.subplot(3, 4, 4), plt.imshow(GB_image, cmap='gray'), plt.title('Gray Gaussian blur')
-
+    GB_image = cv2.GaussianBlur(grayscale_image, kernel_size, 0)
+    plt.subplot(2, 6, 3), plt.imshow(GB_image), plt.title('Gaussian blur')
+    plt.subplot(2, 6, 4), plt.imshow(GB_image, cmap='gray'), plt.title('Gray Gaussian blur')
     # [X] BINARY GAUSSIAN BLUR
     threshold_value = filters.threshold_otsu(GB_image)
     binary_image = GB_image > threshold_value
-    plt.subplot(3, 4, 5), plt.imshow(binary_image, cmap='binary'), plt.title('Binary Gaussian blur')
-
-    # [] MASK
-    threshold_value = filters.threshold_otsu(gray_image)
-    binary_mask = (gray_image > threshold_value).astype(np.uint8) * 255
-    masked_image = cv2.bitwise_and(image, image, mask=binary_mask)
-    plt.subplot(3, 4, 6), plt.imshow(masked_image[:, :, ::-1]), plt.title('Mask')
-
-    # ROI OBJECTS
+    plt.subplot(2, 6, 5), plt.imshow(binary_image, cmap='binary'), plt.title('Binary Gaussian blur')
+    ## MASKS
+    # [] HSV MASK
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    plt.subplot(2, 6, 6), plt.imshow(hsv_image), plt.title('HSV Mask')
+    # [] RGB MASK
+    ft_rgb_mask(image, grayscale_image)
+    # [] COLOR MASK
+    ft_color_mask(image)
+    # [] ROI OBJECTS
+    ft_roi_objects(image)
     # ANALYZE OBJECTS
+    ft_analyze_objects(image)
     # PSEUDOLANDMARKS
-    
-    # // FACULTATIF
-    # COLOR HISTOGRAM
-    # HISTOGRAM EQUALIZATION
-    # CLAHE
-    # ADAPTIVE THRESHOLD
-    # OTSU THRESHOLD
-    # BINARY THRESHOLD
-    # MASKING
+    ft_Pseudolandmarks(image)
 
-    # DISPLAY
     plt.tight_layout()
     plt.show()
-    pass
 
 def main():
     # NO ARGUMENTS
