f11833ba730d6dfd21cef25d545aa2aebcd6241a
m0saan <moboustt@student.1337.ma>
2023-11-18 00:17:34 +0100

    almost finishing ...

--- predict.py
+++ predict.py
@@ -1,12 +1,25 @@
 from PIL import Image
 import sys
 import torch
-import os
-import sys
 import numpy as np
-import matplotlib.pyplot as plt
-import pathlib
+import pickle
 import timm
+from torchvision.transforms import (
+    Compose,
+    Resize,
+    CenterCrop,
+    ToTensor,
+    Lambda,
+    ToPILImage,
+    Normalize,
+)
+
+reverse_transform = Compose([
+     Lambda(lambda t: t.permute(1, 2, 0)),
+     Lambda(lambda t: t * 255.),
+     Lambda(lambda t: t.numpy().astype(np.uint8)),
+     ToPILImage(),
+])
 
 
 def accuracy(outputs, labels):
@@ -20,7 +33,7 @@ def accuracy(outputs, labels):
     Returns:
     float: The accuracy of the model.
     """
-    
+
     _, preds = torch.max(outputs, dim=1)
     correct = (preds == labels).float()
     accuracy = correct.sum() / len(correct)
@@ -28,19 +41,28 @@ def accuracy(outputs, labels):
 
 
 def predict(model, image_path, device, labels):
+    # Open the image file
     img = Image.open(image_path)
     model = model.eval()
 
     data_config = timm.data.resolve_model_data_config(model)
-    transforms = timm.data.create_transform(**data_config, is_training=False)
+    transform = Compose([
+        Resize(size=256, max_size=None,),
+        CenterCrop(size=(224, 224)),
+        ToTensor(),
+        Normalize(mean=data_config['mean'], std=data_config['std']),
+    ])
+    img_transformed = transform(img).unsqueeze(0)
+    img_transformed = img_transformed.to(device)
 
-    output = model(transforms(img).unsqueeze(0).to(device))
+    with torch.no_grad():
+        output = model(img_transformed)
 
-    top5_probabilities, top5_class_indices = torch.topk(output.softmax(dim=1) * 100, k=5)
-    pred_idx = top5_class_indices[0,0].item()
-    print(f'Predicted: {labels[pred_idx]}, idx: {pred_idx}')
-    return labels[pred_idx], transforms(img).numpy().transpose(1,2,0)
-    
+    probabilities = torch.nn.functional.softmax(output, dim=1)
+    top_prediction = probabilities.argmax(1).item()
+    ret_img = img_transformed.squeeze(0).permute(1, 2, 0).cpu().numpy()
+    return labels[top_prediction], ret_img
+
 
 def main():
 
@@ -48,51 +70,29 @@ def main():
         print("Usage: predict.py <path>")
         sys.exit(1)
     image_path = sys.argv[1]
-    
-    label2int = {
-        'Apple_rust': 0,
-        'Apple_Black_rot': 1,
-        'Grape_Esca': 2,
-        'Apple_healthy': 3,
-        'Grape_healthy': 4,
-        'Grape_spot': 5,
-        'Apple_scab': 6,
-        'Grape_Black_rot': 7
-        }
-    int2label = {v:k for k,v in label2int.items()}
+
+    with open('label_dict.pkl', 'rb') as f:
+        label2int = pickle.load(f)
+    int2label = {v: k for k, v in label2int.items()}
+
+    print(int2label)
+
     num_classes = 8
-    
-    model = timm.create_model('convnext_small.fb_in22k', pretrained=True, num_classes=num_classes)
+
+    model = timm.create_model('convnext_small.fb_in22k', pretrained=True,
+                              num_classes=num_classes)
+    model.load_state_dict(torch.load('model.pth'))
 
     # Move the model to GPU if available
-    device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'mps' if torch.backends.mps.is_available() \
+        else 'cuda' if torch.cuda.is_available() else 'cpu'
     model = model.to(device)
     print(f'>>>device: {device}')
 
-    prediction, preprocessed_img = predict(model, image_path=image_path, device=device, labels=int2label)
-    
-    # Create a figure with two subplots
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+    prediction, preprocessed_img = predict(model, image_path=image_path,
+                                           device=device, labels=int2label)
+    print(f'>>>prediction: {prediction}')
 
-    # Display original image
-    axes[0].imshow(Image.open(image_path))
-    axes[0].set_title("Original Image")
-    axes[0].axis('off')
-
-    # Display transformed/preprocessed image
-    axes[1].imshow(preprocessed_img.astype('uint8'))
-    axes[1].set_title("Transformed Image")
-    axes[1].axis('off')
-
-    # Add classification label
-    fig.text(0.5, 0.01,
-             f"DL classification\nClass predicted: {prediction}",
-             ha="center",
-             fontsize=12,
-             bbox={"facecolor":"green", "alpha":0.3, "pad":3})
-
-    plt.tight_layout(pad=3.0)
-    plt.show()
 
 if __name__ == "__main__":
     main()
--- train.py
+++ train.py
@@ -4,62 +4,86 @@ from torch import optim
 from torch.utils.data import DataLoader, random_split
 from torchvision import transforms
 from torch.utils.data import Dataset
-from torchvision import models
-
-from PIL import Image
-import os
 import sys
-import numpy as np
 import matplotlib.pyplot as plt
 import pathlib
-from random import shuffle
 import timm
+import os
+import pickle
+from PIL import Image
+import torchvision.transforms.functional as TF
+import zipfile
+from torchvision.transforms import Resize
 
 
 class ImageDataset(Dataset):
-    def __init__(self, directory, transform=None):
-        self.directory = directory
+    def __init__(self, root_dir, transform=None, save_transformed_images=True):
+        self.root_dir = root_dir
         self.transform = transform
-
-        self.image_paths = []
+        self.save_transformed_images = save_transformed_images
+        self.aug_images_dir = 'aug_images'
+        self.images = []
         self.labels = []
-        self.label_to_int = {}
 
-        label_int = 0
-        for label in os.listdir(directory):
-            label_dir = os.path.join(directory, label)
-            if os.path.isdir(label_dir):
-                if label not in self.label_to_int:
-                    self.label_to_int[label] = label_int
-                    label_int += 1
-                for image_name in os.listdir(label_dir):
-                    self.image_paths.append(os.path.join(label_dir, image_name))
-                    self.labels.append(self.label_to_int[label])
-        print(self.label_to_int)
+        if self.save_transformed_images\
+                and not os.path.exists(self.aug_images_dir):
+            os.makedirs(self.aug_images_dir)
+
+        label_dict = {
+            "Apple_Black_rot": 0,
+            "Apple_healthy": 1,
+            "Apple_rust": 2,
+            "Apple_scab": 3,
+            "Grape_Black_rot": 4,
+            "Grape_Esca": 5,
+            "Grape_healthy": 6,
+            "Grape_spot": 7
+        }
+
+        for label, idx in label_dict.items():
+            folder = os.path.join(root_dir, label)
+            for img_file in os.listdir(folder):
+                self.images.append(os.path.join(folder, img_file))
+                self.labels.append(idx)
+
+        with open('label_dict.pkl', 'wb') as f:
+            pickle.dump(label_dict, f)
 
     def __len__(self):
-        return len(self.image_paths)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        image_path = self.image_paths[idx]
-        image = Image.open(image_path).convert('RGB')
+        img_path = self.images[idx]
+        image = Image.open(img_path).convert("RGB")
+        label = self.labels[idx]
 
         if self.transform:
             image = self.transform(image)
+            if self.save_transformed_images:
+                transformed_image_path = os.path.join(
+                    self.aug_images_dir, f'transformed_{idx}.jpg')
+                TF.to_pil_image(image).save(transformed_image_path)
 
-        label = self.labels[idx]
         return image, label
 
+    def zip_transformed_images(self):
+        zipf = zipfile.ZipFile('aug_images.zip', 'w', zipfile.ZIP_DEFLATED)
+        for root, dirs, files in os.walk(self.aug_images_dir):
+            for file in files:
+                zipf.write(
+                        os.path.join(root, file),
+                        os.path.relpath(
+                            os.path.join(root, file),
+                            os.path.join(self.aug_images_dir, '..')))
+        zipf.close()
+
+
 data_transforms = transforms.Compose([
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomVerticalFlip(),
-    transforms.RandomRotation(20),
-    transforms.RandomAffine(degrees=0, shear=10, scale=(0.8,1.2)),
-    transforms.RandomCrop((200, 200)),
-    transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+    Resize(size=256),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
+
 
 def accuracy(outputs, labels):
     """
@@ -80,6 +104,7 @@ def accuracy(outputs, labels):
     accuracy = correct.sum() / len(correct)
     return accuracy
 
+
 def fit(epochs, model, loss_func, opt, train_dl, valid_dl, device):
     recorder = {'tr_loss': [], 'val_loss': [], 'tr_acc': [], 'val_acc': []}
     for epoch in range(epochs):
@@ -92,14 +117,11 @@ def fit(epochs, model, loss_func, opt, train_dl, valid_dl, device):
             loss.backward()
             opt.step()
             opt.zero_grad()
-
             train_loss += loss.item() * len(xb)
             train_acc += accuracy(pred, yb).item() * len(xb)
             train_count += len(xb)
-
         recorder['tr_loss'].append(train_loss / train_count)
         recorder['tr_acc'].append(train_acc / train_count)
-
         model.eval()
         val_loss, val_acc, val_count = 0., 0., 0
         with torch.no_grad():
@@ -107,22 +129,23 @@ def fit(epochs, model, loss_func, opt, train_dl, valid_dl, device):
                 xb, yb = xb.to(device), yb.to(device)
                 pred = model(xb)
                 loss = loss_func(pred, yb)
-
                 val_loss += loss.item() * len(xb)
                 val_acc += accuracy(pred, yb).item() * len(xb)
                 val_count += len(xb)
-
         recorder['val_loss'].append(val_loss / val_count)
         recorder['val_acc'].append(val_acc / val_count)
-
-        print(f"Epoch {epoch + 1}/{epochs} - Loss: {train_loss / train_count:.4f}, "
-              f"Acc: {train_acc / train_count:.4f}, Val Loss: {val_loss / val_count:.4f}, "
+        print(f"Epoch {epoch + 1}/{epochs} - Loss: \
+            {train_loss / train_count:.4f}, "
+              f"Acc: {train_acc / train_count:.4f}, \
+                  Val Loss: {val_loss / val_count:.4f}, "
               f"Val Acc: {val_acc / val_count:.4f}")
 
     # Plotting outside the loop
     fig, ax = plt.subplots(figsize=(14, 7))
-    ax.plot(recorder['tr_loss'], label='Train Loss', c='#983FFF', linestyle='-')
-    ax.plot(recorder['val_loss'], label='Validation Loss', c='#FF9300', linestyle='--')
+    ax.plot(recorder['tr_loss'], label='Train Loss',
+            c='#983FFF', linestyle='-')
+    ax.plot(recorder['val_loss'], label='Validation Loss',
+            c='#FF9300', linestyle='--')
     ax.set_xlabel('Epochs')
     ax.set_ylabel('Loss')
     ax.set_title('Training and Validation Loss')
@@ -135,59 +158,41 @@ def fit(epochs, model, loss_func, opt, train_dl, valid_dl, device):
 
 
 if __name__ == "__main__":
-    
     if len(sys.argv) != 2:
         print("Usage: train.py <path>")
         sys.exit(1)
-    
     path = sys.argv[1]
-    
     model = timm.create_model('convnext_small.fb_in22k', pretrained=True)
-
     data_config = timm.data.resolve_model_data_config(model)
     transforms = timm.data.create_transform(**data_config, is_training=False)
-    
-    dataset = ImageDataset(directory=path, transform=transforms)
-    label2int = {'Apple_rust': 0, 'Apple_Black_rot': 1, 'Grape_Esca': 2, 'Apple_healthy': 3, 'Grape_healthy': 4, 'Grape_spot': 5, 'Apple_scab': 6, 'Grape_Black_rot': 7}
-    int2label = {v:k for k,v in label2int.items()}
-
+    dataset = ImageDataset(root_dir=path, transform=data_transforms)
     # Determine the lengths of the splits
     train_len = int(0.8 * len(dataset))  # 80% of the dataset for training
     val_len = len(dataset) - train_len  # The rest for validation
-
     train_dataset, val_dataset = random_split(dataset, [train_len, val_len])
-    
     train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
-    
-
     # Freeze all layers
     for param in model.parameters():
         param.requires_grad = False
-
     num_classes = 8
     model.head.fc = nn.Linear(model.head.fc.in_features, num_classes)
-
     # Move the model to GPU if available
-    device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'mps' if torch.backends.mps.is_available() \
+        else 'cuda' if torch.cuda.is_available() else 'cpu'
     model = model.to(device)
     print(f'>>>device: {device}')
-
-    
     xb, yb = next(iter(train_loader))
     xb, yb = xb.to(device), yb.to(device)
-
     print(f'>>>xb shape: {xb.shape}')
     print(f'>>>yb shape: {yb.shape}')
     print(f'>>>xb mean: {xb.mean()}, xb std: {xb.std()}')
-
-    batch_size=64
+    batch_size = 64
     loss_fn = nn.CrossEntropyLoss()
     opt = optim.AdamW(model.head.fc.parameters(), lr=0.005)
-    epochs = 5
-    
-    recorder = fit(epochs, model, loss_fn, opt, train_loader, val_loader, device=device)
-    
+    epochs = 1
+    recorder = fit(epochs, model, loss_fn, opt,
+                   train_loader, val_loader, device=device)
     print(f'saving model to {pathlib.Path.cwd()}')
     torch.save(model.state_dict(), 'model.pth')
-    
+    dataset.zip_transformed_images()
